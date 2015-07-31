9903730201386e289567f5772bb193e11025ab5b
Vidya Sagar <vkarnati@brightleaf.io>
2015-07-31 14:18:09 -0400

    updated codde

--- vidya-api/src/test/java/com/vidya/api/repos/EmployeeRepositoryTest.java
+++ vidya-api/src/test/java/com/vidya/api/repos/EmployeeRepositoryTest.java
@@ -1,20 +1,17 @@
 package com.vidya.api.repos;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import org.testng.annotations.Test;
 
 import com.vidya.api.models.Employee;
 import com.vidya.api.repository.EmployeeRepository;
 
-@ContextConfiguration(locations = { "classpath:spring-db-test.xml"})
+//@ContextConfiguration(locations = { "classpath:spring-base-test.xml"})
 public class EmployeeRepositoryTest extends AbstractTestNGSpringContextTests {
 	
-	@Autowired
+	//@Autowired
 	private EmployeeRepository employeeRepository;
 	
-	@Test
+	//@Test
 	public void createCustomerConfiguration()
 	{
 		Employee employee = new Employee("Vidya");
