d224e7db850a22e4d18b79d1cc3d62f4afd87b02
Vidya Sagar <vkarnati@brightleaf.io>
2015-11-18 15:13:56 -0500

    fixed build issues

--- src/main/java/test/TestClass.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-public class TestClass
-{
-	public TestClass(String str1,String str2,String str3,String str4)
-	{
-		System.out.println(str1+" : "+str2+" : "+str3+" : "+str4);
-	}
-}
